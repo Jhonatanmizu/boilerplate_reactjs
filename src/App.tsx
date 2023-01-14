07480b34c65498a58b49f916391b11ce0f708886
jhonatanmizu <natanjesuss20@gmail.com>
2023-01-14 13:54:56 -0300

    :recycle: deleted index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-export default function App() {
+import React from 'react';
+import GlobalStyle from './styles/global';
+const App: React.FC = () => {
   return (
     <div>
+      <GlobalStyle />
       <h1>REACTJS BOILERPLATE BY MIZUGAWA</h1>
     </div>
   );
-}
+};
+export default App;
